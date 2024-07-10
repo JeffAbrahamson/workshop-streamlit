0b0c19ebc69c3297b5b11bcc3a9ef9a67bb07bc9
Jeff Abrahamson <jeff@p27.eu>
2024-07-10 21:26:30 +0200

    do a query

---
+++ step3_2.py
@@ -0,0 +1,26 @@
+import os
+
+from langchain_community.agent_toolkits import (
+    SQLDatabaseToolkit,
+    create_sql_agent,
+)
+from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_openai import ChatOpenAI
+
+db = SQLDatabase.from_uri("sqlite:///power-generation.sqlite")
+
+toolkit = SQLDatabaseToolkit(db=db, llm=ChatOpenAI(temperature=0))
+context = toolkit.get_context()
+tools = toolkit.get_tools()
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo", temperature=0, api_key=os.getenv("OPENAI_API_KEY")
+)
+agent_executor = create_sql_agent(
+    llm, db=db, agent_type="openai-tools", verbose=True
+)
+
+agent_executor.invoke(
+    "How many countries are there in the power generation table"
+)
+agent_executor.invoke("How many rows are there in the power generation table")
